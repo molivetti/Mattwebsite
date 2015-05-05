98411642aa077077093311d0ab684499bb696def
mwolivet <mwolivet@g.cofc.edu>
2015-05-04 23:30:24 -0400

    SIMPLE - Found new blog thingy

---
+++ simple/js/include-sections.js
@@ -0,0 +1,8 @@
+var thisPathname = document.location.pathname;
+
+$("head").load("head.html");
+$(".nav-container").load("nav.html");
+//$(".header-container").load("header.html", );
+$(".footer-container").load("footer.html");
+
+$("body").show();
